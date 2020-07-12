f980be2a0511b7113e26c029e2de0e39aff40243
Andreas Linnert <andreas@linnertmedia.de>
2020-07-12 23:09:37 +0200

    Typ-Parameter der Artikel-Route inline platziert

--- src/plugins/routes/blog.tsx
+++ src/plugins/routes/blog.tsx
@@ -3,16 +3,14 @@ import React from 'react'
 import { defaultTemplate } from '../../templates/default'
 
 export const blogPlugin: FastifyPluginCallback = async (app, options, next) => {
-  interface GetSlug {
+  app.get<{
     Params: { slug: string }
-  }
-
-  app.get<GetSlug>('/:slug', async (request, reply) => {
+  }>('/:slug', async (request, reply) => {
     return defaultTemplate(
       <>
         <a href="/">Startseite</a>
         <h1>{request.params.slug}</h1>
-        <p>Diese Seite verwendet JSX, &quot;renderTemplate()&quot; und ein Fastify-Plugin</p>
+        <p>Diese Seite verwendet TSX und ein Fastify-Plugin.</p>
       </>
     )
   })

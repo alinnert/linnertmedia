946ab596ab763d97e887af0bc3e6d6d49f609276
Andreas Linnert <andreas@linnertmedia.de>
2020-07-13 21:26:30 +0200

    Fix für ts-node-dev und pretty-print Fastify-Logs
    Potentieller Fix für unzuverlässiges Verhalten von `ts-node-dev`.
    Log-Ausgaben von Fastify in der Konsole sind nun lesbar statt JSON.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import * as path from 'path'
-import fastify from 'fastify'
+import fastify, { FastifyLoggerOptions } from 'fastify'
 import fastifyStatic from 'fastify-static'
+import * as path from 'path'
 import { htmlRoutesPlugin } from './plugins/routes'
 
-const app = fastify({ logger: true })
+const logger: FastifyLoggerOptions & { prettyPrint: boolean } = {
+  level: 'debug',
+  prettyPrint: true
+}
+
+const app = fastify({ logger })
 const port = process.env.NODE_ENV === 'development' ? 8000 : 80
 const staticFilesRoot = path.resolve(process.cwd(), 'build/assets')
 

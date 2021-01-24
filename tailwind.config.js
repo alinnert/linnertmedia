a72f01775b57764eab78e91b5081a29be8a00920
Andreas Linnert <andreas@linnertmedia.de>
2021-01-24 23:27:16 +0100

    Schriften hinzugefügt und Typo im CSS angepasst

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ module.exports = {
       current: "currenColor",
       brand: colors.cyan,
       gray: colors.gray,
-      green: colors.emerald
+      green: colors.emerald,
+    },
+    fontFamily: {
+      heading: ["Montserrat", "sans-serif"],
+      sans: ["Open Sans", "sans-serif"],
+      mono: ["IBM Plex Mono", "monospace"],
     },
     extend: {
       typography: (theme) => ({
@@ -20,8 +25,29 @@ module.exports = {
           css: {
             maxWidth: "none",
             code: {
-              color: theme('colors.brand.800')
-            }
+              color: theme("colors.brand.800"),
+            },
+            h2: {
+              fontFamily: theme("fontFamily.heading").toString(),
+              fontWeight: "400",
+              position: "relative",
+              "&::before": {
+                content: '"# "',
+                color: theme("colors.gray.500"),
+                [`@media (min-width: ${theme("screens.lg")})`]: {
+                  content: '"#"',
+                  position: "absolute",
+                  left: "-1.2em",
+                },
+              },
+            },
+          },
+        },
+        lg: {
+          css: {
+            h2: {
+              fontSize: theme("fontSize.2xl")[0],
+            },
           },
         },
       }),
